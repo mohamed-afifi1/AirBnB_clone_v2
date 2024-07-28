27fcd54f8622af35d052e126299cf38a13dad769
root <root@DESKTOP-HU468P5.localdomain>
2024-07-28 13:41:13 +0300

    c is fun

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+hello in flask app
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """Return a friendly greeting."""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    """Return a friendly greeting for HBNB."""
+    return 'HBNB'
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is(text):
+    """return c and text given in route"""
+    text = text.replace('_', ' ')
+    return 'C ' + text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
