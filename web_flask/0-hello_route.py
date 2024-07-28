c6861e7c3486988cc7a31c33b3d8c3b3e08e9450
root <root@DESKTOP-HU468P5.localdomain>
2024-07-28 13:14:29 +0300

    wellcome flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,10 +7,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_world():
     """Return a friendly greeting."""
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
