8abdff0042621d644f5dc04d0407c64ff60639df
root <root@DESKTOP-HU468P5.localdomain>
2024-07-28 14:06:38 +0300

    num

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+hello in flask app
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """Return a friendly greeting."""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    """Return a friendly greeting for HBNB."""
+    return 'HBNB'
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is(text):
+    """return c and text given in route"""
+    text = text.replace('_', ' ')
+    return 'C ' + text
+
+
+@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python", strict_slashes=False)
+def python_is(text="is cool"):
+    """return python and text given in route"""
+    text = text.replace('_', ' ')
+    return 'Python ' + text
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_is(n):
+    """Return the number provided in the route."""
+    return str(n) + " is a number"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
